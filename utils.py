f092fa2d6d343843cd1d367ffc98eeafecbcaa37
Doradoru <doradoru001@gmail.com>
2025-07-20 09:18:52 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,40 +1,14 @@
 import gspread
-from datetime import datetime
 from google.oauth2.service_account import Credentials
+import streamlit as st
+import json
+from datetime import datetime
 
-# --- Google Sheets Setup ---
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-creds = Credentials.from_service_account_file("credentials.json", scopes=scope)
+
+# For Streamlit secrets.toml
+creds_dict = json.loads(st.secrets["GOOGLE_SHEET_JSON"])
+creds = Credentials.from_service_account_info(creds_dict, scopes=scope)
+
 client = gspread.authorize(creds)
 sheet = client.open("AniGPT_DB")
-
-# --- Ensure 'Users' Tab ---
-def ensure_users_tab():
-    try:
-        ws = sheet.worksheet("Users")
-        headers = ws.row_values(1)
-        required = ["Name", "Password", "Created At"]
-        if headers != required:
-            ws.clear()
-            ws.append_row(required)
-    except gspread.exceptions.WorksheetNotFound:
-        ws = sheet.add_worksheet(title="Users", rows=100, cols=3)
-        ws.append_row(["Name", "Password", "Created At"])
-
-ensure_users_tab()
-
-# --- Login Function ---
-def login_user(name, password):
-    ws = sheet.worksheet("Users")
-    users = ws.get_all_records()
-    for user in users:
-        if "Name" in user and "Password" in user:
-            if user["Name"] == name and user["Password"] == password:
-                return True
-    return False
-
-# --- Optional: Signup Function ---
-def signup_user(name, password):
-    ws = sheet.worksheet("Users")
-    ws.append_row([name, password, datetime.now().strftime("%Y-%m-%d %H:%M:%S")])
-    return True
